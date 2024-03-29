867ebb0992c156bc48343e07e667ffd1e1edfb0b
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-30 04:10:22 +0900

    feat: 라우팅 설정

--- stepstory/src/index.js
+++ stepstory/src/index.js
@@ -1,50 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import Login from './components/Login/login.jsx'; // Login 컴포넌트를 import 해야 합니다.
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-// import Socialsign from "./components/SocialSign/Socialsign.jsx";
-// import Sign from "./components/Sign/Sign.jsx";
-import Main from "./pages/Main/Main";
-import FriendStory from "./components/FriendStory/FriendStory.jsx";
-import MyStory from './components/MyStory/MyStory.jsx';
-import ViewPost from './pages/ViewPost/ViewPost.jsx';
-import PostStep1 from "./pages/PostStep1/PostStep1.jsx";
-import PostStep2 from "./pages/PostStep2/PostStep2.jsx"
-import PostStep3 from "./pages/PostStep3/PostStep3.jsx"
-<<<<<<< HEAD
-import MainSeoul from './components/Main/MainSeoul/MainSeoul.jsx';
-=======
-import Seoul from './components/MainMap/Seoul/Seoul.jsx';
->>>>>>> 265beb4 (fix: page 단위 구분)
+import Router from './routers.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        {/* <Login /> */}
-        {/* <Socialsign /> */}
-        {/* <Sign /> */}
-        {/*<PostStep1 />*/}
-        {/*<App />*/}
-        {/* {<Main />} */}
-        {/* <FriendStory/> */}
-        {/* <MyStory /> */}
-        {/* <Main /> */}
-        {/* <Seoul /> */}
-        <FriendStory/>
-        {/* {<ViewPost/>} */}
-        {/* {<PostStep2/>} */}
-        {/* {<PostStep3/>} */}
-        {/* <Main /> */}
-        {/* <FriendStory/> */}
-        {/* <MyStory /> */}
-        {/*<MainSeoul />*/}
+        <Router />
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- stepstory/src/routers.jsx
+++ stepstory/src/routers.jsx
@@ -1,29 +1,24 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/login";
+import Sign from "./components/Sign/sign"
 import Main from "./pages/Main/Main";
 import PostStep1 from "./pages/PostStep1/PostStep1";
 import PostStep2 from "./pages/PostStep2/PostStep2";
 import PostStep3 from "./pages/PostStep3/PostStep3";
+import Seoul from "./components/MainMap/Seoul/Seoul";
 
-
-const Layout = () => {
-  return (
-    <>
-      <Wrapper>
-        <Outlet />
-      </Wrapper>
-    </>
-  );
-};
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-            <Route path="login" element={<Login />} />
-        </Route>
+        <Route path="/" element={<Main />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Sign />} />
+        <Route path="/seoul" element={<Seoul />} />
+        <Route path="/post-step1" element={<PostStep1 />} />
+        <Route path="/post-step2" element={<PostStep2 />} />
+        <Route path="/post-step3" element={<PostStep3 />} />
       </Routes>
     </BrowserRouter>
   );
