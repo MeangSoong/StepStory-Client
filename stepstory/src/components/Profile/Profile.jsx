9ee16debb15ce216d182f69de956bf9b4d0b5d40
MeangSoong <149292274+MeangSoong@users.noreply.github.com>
2024-03-31 08:43:57 +0900

    refactor: 로그인 라우터 설정, axios를 통한 user 정보 가져오고 로컬 스토리에 저장

--- stepstory/src/components/Profile/Profile.jsx
+++ stepstory/src/components/Profile/Profile.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import './profileStyle.css'
 import defaultProfileImg from '../../assets/images/defaultprofile.png';
 
-export default function Profile ()  {
+export default function Profile ({profile_image_url, self_intro, nickname})  {
+
+    //props로 데이터 전달받아서 나머지 정보 뿌려주는 로직(프로필 이미지, 닉네임, 한줄 소개)
+
     return (
         <div className='userProfile'>
             <div className='userProfileRealArea'>
                 <div className='userProfileImgAndName'>
                     <div className='userProfileImg'>
-                        <img src={defaultProfileImg} alt='userProfileImg' />
+                        <img src={profile_image_url} alt='userProfileImg' />
                     </div>
                     <div className='userProfileName'>
                         <p>NAME</p>
                         <div className='userName'>
-                            의왕시 물주먹
+                            {nickname}
                         </div>
                     </div>
                 </div>
                 <div className='userProfileIntro'>
                     <div id='INTRODUCE'><p>INTRODUCE</p></div>
                     <text className='userIntro'>
-                        가나다라마바사아자차카타파하아자라자바하아어고르다허드엏루아니낭런디
+                        {self_intro}
                     </text>
                 </div>
             </div>
--- stepstory/src/pages/Main/Main.jsx
+++ stepstory/src/pages/Main/Main.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 import Footer from '../../components/Footer/footer';
 import LeftPane from '../../components/LeftPane/LeftPane';
 import RightPane from '../../components/RightPane/RightPane';
+import {Cookies} from 'react-cookie';
+import axios from 'axios';
+
 
 const Main = () => {
 
-    const loggedInInfo = {isLoggedIn: false};
+    const [loggedInInfo, setLoggedInfo] = useState(false);
     const pageInfo = {page: 'main'};
+
+    // local storage 한번 확인하고-> 엑세스 토큰(쿠키 안에 있음) -> 이래도 없음면 false처리, 있으면, back에다가 유저정보 확인 정보 확인 가져와 true 전환
+    
+    //localstorage 확인 logic
+    if(localStorage.getItem('userId') !==null ){
+        const profile_image_url = localStorage.getItem("profile_image_url");
+        const self_intro =  localStorage.getItem("self_intro");
+        const userId = localStorage.getItem("userId");
+        const nickname = localStorage.getItem("nickname");
+
+        setLoggedInfo(true);
+    }
+    //local storage에 없는경우 아래 실행
+    else
+    {//쿠키있는지 확인
+    const cookies = new Cookies();    
+    const accessToken = cookies?.get("access_token");
+    if(accessToken !== null){
+        const headers = {
+            Authorization: `Bearer ${accessToken}`
+        };
+
+        //respose에 application/json 형식으로 정보가 날라온다.
+        const response = axios.get(`users/user`, {
+            headers
+        });
+
+        if(response ==null){
+            console.log('login fail');
+        }
+        else{
+            //로그인 된 leftPane 으로 변경
+            setLoggedInfo(true);
+
+            const profile_image_url = response.data.profile_image_url;
+            const self_info = response.data.self_info;
+            const userId = response.data.userId;
+            const nickname = response.data.nickname;
+
+            //local storage에서 저장, 비밀번호를 제외한 모든걸 저장하는게 좋다.(애초에 안 온데 ㅠ)
+            localStorage.setItem("profile_image_url",profile_image_url);
+            localStorage.setItem("self_intro",self_info);
+            localStorage.setItem("userId",userId);
+            localStorage.setItem("nickname",nickname);
+
+
+        }
+    }}
+    
+    
+
+
+
 
     return (
         <div className="main">
