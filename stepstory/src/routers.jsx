8f2a0c5f5a87e97ecf91801713d5f9b96332d23a
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-31 17:11:38 +0900

    fix: 충돌 해결

--- stepstory/src/routers.jsx
+++ stepstory/src/routers.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/login";
 import Sign from "./components/Sign/sign"
 import Main from "./pages/Main/Main";
+import MainSeoul from "./pages/Main/MainSeoul/MainSeoul";
 import PostStep1 from "./pages/PostStep1/PostStep1";
 import PostStep2 from "./pages/PostStep2/PostStep2";
 import PostStep3 from "./pages/PostStep3/PostStep3";
-import SeoulMap from "./components/SubMap/Seoul/SeoulMap";
+
+
 
 export default function Router() {
   return (
@@ -14,6 +16,7 @@ export default function Router() {
         <Route path="/" element={<Main />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Sign />} />
+        <Route path="/seoul" element={<MainSeoul />} />
         <Route path="/post-step1" element={<PostStep1 />} />
         <Route path="/post-step2" element={<PostStep2 />} />
         <Route path="/post-step3" element={<PostStep3 />} />
