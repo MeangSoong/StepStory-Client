542790a5ba1e548be2f96e24a27415316b006ab8
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-31 16:51:09 +0900

    fix: 중복되지 않았을 때의 메세지 추가

--- stepstory/src/components/Sign/sign.jsx
+++ stepstory/src/components/Sign/sign.jsx
@@ -131,6 +131,7 @@ const RightPane = () => {
                     <input ref={userIdRef} type="text" placeholder="아이디를 입력하세요" className="id-input" onChange={(e) => setUserId(e.target.value)} />
                     <button onClick={() => checkDuplicate(userId, 'userId')} className='btn-duplicate-check'>중복확인</button>
                     {userIdChecked && userIdDuplicate && <div style={{ color: 'red', marginTop: '3px' }}>이미 사용 중인 아이디입니다.</div>}
+                    {userIdChecked && !userIdDuplicate && <div style={{ color: 'green', marginTop: '3px' }}>사용 가능한 아이디입니다.</div>}
                 </div>
                     <input ref={passwordRef} type="password" placeholder="비밀번호를 입력하세요" className="pw-input" onChange={(e) => setPassword(e.target.value)} />
                     <input ref={confirmPasswordRef} type="password" placeholder="비밀번호를 한번 더 입력하세요" className="pw-input" onChange={(e) => setConfirmPassword(e.target.value)} />
@@ -140,6 +141,7 @@ const RightPane = () => {
                     <input ref={nicknameRef} type="text" placeholder="닉네임 입력" className="nickname-input" onChange={(e) => setNickname(e.target.value)} />
                     <button onClick={() => checkDuplicate(nickname, 'nickname')} className='btn2'>중복확인</button>
                     {nicknameChecked && nicknameDuplicate && <div style={{ color: 'red', marginTop: '3px' }}>이미 사용 중인 닉네임입니다.</div>}
+                    {nicknameChecked && !nicknameDuplicate && <div style={{ color: 'green', marginTop: '3px' }}>사용 가능한 닉네임입니다.</div>}
                 </div>
                 {nicknameChecked && (nicknameDuplicate ? <div style={{ color: 'red' }}>이미 사용 중인 닉네임입니다.</div> : <div style={{ color: 'green' }}>사용 가능한 닉네임입니다.</div>)}
                 <br/>
