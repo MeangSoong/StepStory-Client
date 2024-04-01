6d25d0f841156a307b89c8d754ebaa85786499b0
HyeIn <helen6662@naver.com>
2024-04-01 16:08:53 +0900

    fix : poststep2,3 연결 및 css 수정

--- stepstory/src/components/Buttons/NextPageButton/NextPageButton.jsx
+++ stepstory/src/components/Buttons/NextPageButton/NextPageButton.jsx
@@ -1,10 +1,17 @@
 import optionButton from '../../../assets/images/buttons/nextpagebutton.png'
 import './style.css';
+import { useNavigate } from "react-router-dom";
 
 export default function NextPageButton() {
+    const navigate = useNavigate();
+
+    const handlestep3 = () => {
+        navigate('/post-step3');
+    };
+
     return (
-    <button className="next-page-button">
-        <img src={optionButton} alt="다음으로 가기 버튼" width={'40px'} />
-    </button>
+        <button className="next-page-button" onClick={handlestep3}>
+            <img src={optionButton} alt="다음으로 가기 버튼" width={'40px'} />
+        </button>
     );
-  }
+}
